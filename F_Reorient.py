babd2e29f81cf2f2df09da8a42d32b5cd08f9dec
Heng <cain106002@users.noreply.github.com>
2020-05-25 17:07:11 +0800

    FAIRT beta v0.2

--- F_Reorient.py
+++ F_Reorient.py
@@ -20,60 +20,24 @@ subs = os.listdir(tmp_path)
 
 # designed for user with low RAM. For the users who have large RAM, the order 
 # could be changed as: 1)load all 6 keras model, 2) loop all subs only once
-DR_model_tmp = load_model('DR_model_rot12.h5')
+DR_model_tmp = load_model('mov_5_final.h5')
 data_he_tmp = np.zeros([1,64,64,64,1],dtype='float16')
 for sub_name in subs:
     mat_tmp_path = os.path.join(tmp_path,sub_name,'data_he.mat')
     mat_tmp = scio.loadmat(mat_tmp_path)
     data_he_tmp[0,:,:,:,0] = mat_tmp['data4_tmp']
-    rot12_tmp = DR_model_tmp.predict(data_he_tmp)
-    scio.savemat(os.path.join(tmp_path,sub_name,'DR_para_rot12.mat'),{'rot12':rot12_tmp})
+    [rsc1_tmp, rsc2_tmp, rsc3_tmp] = DR_model_tmp.predict(data_he_tmp)
+    scio.savemat(os.path.join(tmp_path,sub_name,'DR_para_mov.mat'),{'mov1':rsc1_tmp,'mov2':rsc2_tmp,'mov3':rsc3_tmp})
     
-DR_model_tmp = load_model('DR_model_rot13.h5')
+DR_model_tmp = load_model('rot_5_final.h5')
 data_he_tmp = np.zeros([1,64,64,64,1],dtype='float16')
 for sub_name in subs:
     mat_tmp_path = os.path.join(tmp_path,sub_name,'data_he.mat')
     mat_tmp = scio.loadmat(mat_tmp_path)
     data_he_tmp[0,:,:,:,0] = mat_tmp['data4_tmp']
-    rot13_tmp = DR_model_tmp.predict(data_he_tmp)
-    scio.savemat(os.path.join(tmp_path,sub_name,'DR_para_rot13.mat'),{'rot13':rot13_tmp})
-    
-DR_model_tmp = load_model('DR_model_rot23.h5')
-data_he_tmp = np.zeros([1,64,64,64,1],dtype='float16')
-for sub_name in subs:
-    mat_tmp_path = os.path.join(tmp_path,sub_name,'data_he.mat')
-    mat_tmp = scio.loadmat(mat_tmp_path)
-    data_he_tmp[0,:,:,:,0] = mat_tmp['data4_tmp']
-    rot23_tmp = DR_model_tmp.predict(data_he_tmp)
-    scio.savemat(os.path.join(tmp_path,sub_name,'DR_para_rot23.mat'),{'rot23':rot23_tmp})
-    
-DR_model_tmp = load_model('DR_model_mov1.h5')
-data_he_tmp = np.zeros([1,64,64,64,1],dtype='float16')
-for sub_name in subs:
-    mat_tmp_path = os.path.join(tmp_path,sub_name,'data_he.mat')
-    mat_tmp = scio.loadmat(mat_tmp_path)
-    data_he_tmp[0,:,:,:,0] = mat_tmp['data4_tmp']
-    mov1_tmp = DR_model_tmp.predict(data_he_tmp)
-    scio.savemat(os.path.join(tmp_path,sub_name,'DR_para_mov1.mat'),{'mov1':mov1_tmp})
-    
-DR_model_tmp = load_model('DR_model_mov2.h5')
-data_he_tmp = np.zeros([1,64,64,64,1],dtype='float16')
-for sub_name in subs:
-    mat_tmp_path = os.path.join(tmp_path,sub_name,'data_he.mat')
-    mat_tmp = scio.loadmat(mat_tmp_path)
-    data_he_tmp[0,:,:,:,0] = mat_tmp['data4_tmp']
-    mov2_tmp = DR_model_tmp.predict(data_he_tmp)
-    scio.savemat(os.path.join(tmp_path,sub_name,'DR_para_mov2.mat'),{'mov2':mov2_tmp})
-    
-DR_model_tmp = load_model('DR_model_mov3.h5')
-data_he_tmp = np.zeros([1,64,64,64,1],dtype='float16')
-for sub_name in subs:
-    mat_tmp_path = os.path.join(tmp_path,sub_name,'data_he.mat')
-    mat_tmp = scio.loadmat(mat_tmp_path)
-    data_he_tmp[0,:,:,:,0] = mat_tmp['data4_tmp']
-    mov3_tmp = DR_model_tmp.predict(data_he_tmp)
-    scio.savemat(os.path.join(tmp_path,sub_name,'DR_para_mov3.mat'),{'mov3':mov3_tmp})
-    
+    [rot12_tmp, rot13_tmp, rot23_tmp] = DR_model_tmp.predict(data_he_tmp)
+    scio.savemat(os.path.join(tmp_path,sub_name,'DR_para_rot.mat'),{'rot12':rot12_tmp,'rot13':rot13_tmp,'rot23':rot23_tmp})
+
 exit()
     
     
